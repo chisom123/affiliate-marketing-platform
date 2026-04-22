c2668ce8eb1a16dd1ba6f9a08360419c20322e14
chisom123 <chisagu5@gmail.com>
2026-04-22 14:56:39 +0100

    Changed images in promo page

--- src/PromoPage.js
+++ src/PromoPage.js
@@ -35,8 +35,8 @@ const detectCountry = () => {
 
 // Replace imageUrl with your real assets. Theme + ratings show as overlay on each card.
 const CAROUSEL_PHOTOS = [
-  { imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ss-web-rate.firebasestorage.app/o/carousel%2Fefaaf606a1373ca54f8d115d272cfb91-2.jpg?alt=media&token=1eb2b05c-0955-4866-bfd7-cb85c86d4dd1', theme: 'McDinner', ratings: 18 },
-  { imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ss-web-rate.firebasestorage.app/o/carousel%2Fcd4b79e5f8015d314f8ec6d2d428c2a5-2.jpg?alt=media&token=5484de1f-d3d2-4705-8b48-10a3a64a836c', theme: 'Night Out', ratings: 22 },
+  { imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ss-web-rate.firebasestorage.app/o/themes%2Fb5e8e167f3bba281d0da3cc6d386f08f-2.jpg?alt=media&token=ca7700c0-9a2e-4c62-b54f-9921dd735d74', theme: 'McDinner', ratings: 18 },
+  { imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ss-web-rate.firebasestorage.app/o/themes%2F07d3c9a11e7539d2410ffba6fc1b969a-2.jpg?alt=media&token=c88d655d-6548-48bf-bd9a-0d1067ea2718', theme: 'Night Out', ratings: 22 },
   { imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ss-web-rate.firebasestorage.app/o/carousel%2F655f4315a879b0dec798fda69813857c-2.jpg?alt=media&token=7cb94bdf-f5ab-4191-9067-f61f399f3991', theme: 'OOTD', ratings: 20 },
   { imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ss-web-rate.firebasestorage.app/o/carousel%2Fed80ca57adbd8b105ebd9ae951e76bd6-2.jpg?alt=media&token=70cf0c6c-10fe-4cb2-ba1a-2ffde7056232', theme: 'Power Nap', ratings: 27 },
   { imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ss-web-rate.firebasestorage.app/o/carousel%2F4d0bf56bcaf63c1ec70ac1fadb616930-2-2.jpg?alt=media&token=b9530927-737b-4f1c-a900-a2f7f8e527fd', theme: 'Getting Ready', ratings: 13 },
@@ -386,7 +386,7 @@ const PromoPage = () => {
                 borderRadius: '50%',
                 animation: 'spin 0.8s linear infinite',
               }} />
-            ) : 'Get Started'}
+            ) : 'Continue'}
           </button>
         </div>
       </div>
@@ -421,6 +421,7 @@ const PromoPage = () => {
                   backgroundColor: '#3B4374',
                   border: 'none', borderRadius: 10,
                   padding: '0 16px',
+                  fontWeight: 'bold',
                   color: 'white', fontSize: 16,
                 }}
               />
